47e476e3ddb88007f2c3734f553329a25e023f04
Léonard Jouve <leonard.jouve@gmail.com>
2026-06-26 11:44:26 +0200

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 apt-get update && apt-get install -y cron
 
 service cron start
@@ -6,4 +8,4 @@ env > /tmp/cron.env
 
 (crontab -l 2>/dev/null; echo "* * * * * /bin/bash -c 'set -a && . /tmp/cron.env && /usr/local/bin/php /var/www/html/admin/cli/cron.php >> /tmp/moodle_cron.log 2>&1'") | crontab -
 
-exec /usr/local/bin/moodle-docker-php-entrypoint
+exec /usr/local/bin/moodle-docker-php-entrypoint apache2-foreground
